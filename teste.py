61703c2f25618fc63d85cb006e0e12b0a495dced
Rafael Martins <rafaelmartinsdandrade@gmail.com>
2022-12-18 23:57:01 -0300

    Update: Faster excel writing
    Writing on excel was changed, and is MUCH BETTER than before.

--- teste.py
+++ teste.py
@@ -15,24 +15,9 @@ qTeste.put(['teste', '4'])
 qTeste.put(['teste', '5'])
 qTeste.put(['teste', '6'])
 
-# excel = pd.read_excel(r"excel.xlsx",'Sheet1',header=1)
 
-# # for linha in excel:
-# for arrEmpresas in excel['Nome da empresa']:
-# 	print(arrEmpresas)
 
-# wb = openpyxl.load_workbook("excel.xlsx")
-# ws = wb.active
-# while not qTeste.empty():
-# 	arrTeste = qTeste.get()
-# 	ws.append(arrTeste)
-# 	wb.save('excel.xlsx')
-
-# wb = load_workbook(r"NF's2.xlsx")
-# sheets = wb.sheetnames
-# sheet1 = wb[sheets[0]]
-# j = i+2
-# sheet1.cell(row = j, column = 19).value = "Cancelada"
-# wb.save("excel.xlsx")
-
-# empresa = excel["Linkedin"]
+excel = pd.ExcelFile("excel.xlsx")
+excel = excel.parse('Sheet1', skiprows=1, usecols = ['Nome da empresa'], index_col=None, na_values=['NA'])
+arrExcelStartups = excel['Nome da empresa']
+arrTemp = arrExcelStartups[1:len(arrExcelStartups)]
