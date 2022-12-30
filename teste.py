f81b80d460d87d73607d2ee79f82d06d2cf3999e
Rafael Martins <rafaelmartinsdandrade@gmail.com>
2022-12-30 10:00:12 -0300

    Add search from google

--- teste.py
+++ teste.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+import urllib.parse
 import openpyxl
 import math as m
 from openpyxl import workbook
@@ -7,17 +9,5 @@ import pandas as pd
 import queue as q
 
 qTeste = q.Queue()
-
-qTeste.put(['teste', '1'])
-qTeste.put(['teste', '2'])
-qTeste.put(['teste', '3'])
-qTeste.put(['teste', '4'])
-qTeste.put(['teste', '5'])
-qTeste.put(['teste', '6'])
-
-
-
-excel = pd.ExcelFile("excel.xlsx")
-excel = excel.parse('Sheet1', skiprows=1, usecols = ['Nome da empresa'], index_col=None, na_values=['NA'])
-arrExcelStartups = excel['Nome da empresa']
-arrTemp = arrExcelStartups[1:len(arrExcelStartups)]
+qTeste.put("+Mu")
+print(qTeste.get())
